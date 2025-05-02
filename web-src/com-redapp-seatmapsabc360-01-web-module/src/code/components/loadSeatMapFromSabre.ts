9f97bd124a6bd103ba11079cd8df296f3d915452
Leonid Kleimann <k_l_b_1963@hotmail.com>
2025-05-02 10:10:22 +0200

    feat: migrate EnhancedSeatMapRQ to v8, add dynamic data, fix POS handling

--- web-src/com-redapp-seatmapsabc360-01-web-module/src/code/components/loadSeatMapFromSabre.ts
+++ web-src/com-redapp-seatmapsabc360-01-web-module/src/code/components/loadSeatMapFromSabre.ts
@@ -23,27 +23,51 @@ export const loadSeatMapFromSabre = async (
     const soapApiService = getService(ISoapApiService);
 
     const passengerXml = passengers
-      .map(
-        (p) => `<Passenger id="${p.value}"><Name><Given>${p.givenName}</Given><Surname>${p.surname}</Surname></Name></Passenger>`
-      )
-      .join('');
+    .map(
+      (p, index) => `
+        <Passenger id="${p.value}">
+          <Name>
+            <Given>${p.givenName.split(' ')[0]}</Given>
+            <Surname>${p.surname}</Surname>
+          </Name>
+          <PTC>ADT</PTC>
+        </Passenger>
+      `
+    )
+    .join('');
 
     const soapPayload = `
-      <EnhancedSeatMapRQ Version="4.0.0" xmlns="http://stl.sabre.com/Merchandising/v4">
-        <SeatMapQueryEnhanced>
-          <RequestType>Payload</RequestType>
-          <FlightSegment>
-            <BookingClass>${segment.bookingClass}</BookingClass>
-            <MarketingCarrier>${segment.marketingCarrier}</MarketingCarrier>
-            <MarketingFlightNumber>${segment.marketingFlightNumber}</MarketingFlightNumber>
-            <DepartureDate>${segment.departureDate}</DepartureDate>
-            <OriginLocation>${segment.origin}</OriginLocation>
-            <DestinationLocation>${segment.destination}</DestinationLocation>
-          </FlightSegment>
-          <Passengers>${passengerXml}</Passengers>
-        </SeatMapQueryEnhanced>
-      </EnhancedSeatMapRQ>
+    <ns4:EnhancedSeatMapRQ xmlns:ns4="http://stl.sabre.com/Merchandising/v8">
+      <ns4:SeatMapQueryEnhanced>
+        <ns4:RequestType>Payload</ns4:RequestType>
+        <ns4:Flight origin="${segment.origin}" destination="${segment.destination}">
+          <ns4:DepartureDate>${segment.departureDate}</ns4:DepartureDate>
+          <ns4:Marketing carrier="${segment.marketingCarrier}">${parseInt(segment.marketingFlightNumber, 10)}</ns4:Marketing>
+        </ns4:Flight>
+        <ns4:CabinDefinition>
+          <ns4:RBD>${segment.bookingClass}</ns4:RBD>
+        </ns4:CabinDefinition>
+        ${passengers
+          .map(
+            (p, index) => `
+          <ns4:FareAvailQualifiers fareBasisCode="TESTFARE" passengerType="ADT">
+            <ns4:TravellerID>${index + 1}</ns4:TravellerID>
+            <ns4:GivenName>${p.givenName.split(' ')[0]}</ns4:GivenName>
+            <ns4:Surname>${p.surname}</ns4:Surname>
+          </ns4:FareAvailQualifiers>
+        `
+          )
+          .join('')}
+        <ns4:POS multiHost="${segment.marketingCarrier}" company="${segment.marketingCarrier}">
+          <ns4:Actual city="${segment.origin}"/>
+          <ns4:PCC>DI9L</ns4:PCC>
+          <ns4:ClientContext clientType="SSW_RES"/>
+        </ns4:POS>
+      </ns4:SeatMapQueryEnhanced>
+    </ns4:EnhancedSeatMapRQ>
     `;
+
+    console.log('🚀 Sending EnhancedSeatMapRQ:\n', soapPayload);
 
     const response = await soapApiService.callSws({
       action: 'EnhancedSeatMapRQ',
